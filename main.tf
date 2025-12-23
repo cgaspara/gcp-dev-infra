58ced8ee655d85c237a1b4263bb9e1e9e5eef827
cgaspara <cgaspara@gmail.com>
2025-12-23 13:36:17 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,12 @@
-.
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_storage_bucket" "lab_bucket" {
+  name          = var.bucket_name
+  location      = var.region
+  storage_class = "STANDARD"
+  force_destroy = true
+}
+
