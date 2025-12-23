63bee2fe22adf6f211f05108125abf0627e4669b
cgaspara <cgaspara@gmail.com>
2025-12-23 13:39:29 -0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,15 @@
+variable "project_id" {
+  description = "Your GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP region"
+  default     = "us-central1"
+}
+
+variable "bucket_name" {
+  description = "Name of the GCS bucket"
+  type        = string
+}
 
